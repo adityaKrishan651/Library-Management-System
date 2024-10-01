9c11715cc6ed0c80714478e00652fe0f74b68f33
adityaKrishan651 <adityakrishan15@gmail.com>
2024-10-01 16:26:24 +0530

    added a print command.

--- manage.py
+++ manage.py
@@ -15,6 +15,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    print("something")
 
 
 if __name__ == '__main__':
